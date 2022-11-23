225bfc642b94a623bf60d33b6e7c275cdaa6b51c
entrydenied <benjamintak17@gmail.com>
2022-11-23 10:54:04 -0800

    Edit for loop & comment tests

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 const cellDiv = document.querySelector('.container');
 
 // Creates a 16x16 Grid
-
-for (let i = 0; i < 257; i++) {
-    const cell = document.createElement('cell');
-    cellDiv.appendChild(cell);
+for (let i = 0; i < 256; i++) {
+  const cell = document.createElement('div');
+  cellDiv.appendChild(cell);
 }
+
 
 
 /** 
@@ -14,4 +14,21 @@ function Grid = () {
     alert
     for (let i = 0; i < 257; i++)
 }
+
+let count = 0;
+
+// Creates a 16x16 Grid
+function Grid(makeGrid) {
+    for (let i = 0; i < 257; i++) {
+        const cell = document.createElement('cell');
+        cellDiv.appendChild(cell);
+        count++;
+    }
+
+    if (count === 256) {
+        
+    }
+}
+console.log(Grid());
+
 */
