e7f9f228b46e7f8ab3866b286b63d374db1ec602
entrydenied <benjamintak17@gmail.com>
2022-11-23 10:25:59 -0800

    Add grid for loop

---
+++ app.js
@@ -0,0 +1,16 @@
+// Global Definitions
+const cellDiv = document.querySelector('.container');
+
+// Creates a 16x16 Grid
+for (let i = 0; i < 257; i++) {
+    const cell = document.createElement('cell');
+    cellDiv.appendChild(cell);
+}
+
+
+/** 
+const Grid = () => {
+    alert
+    for (let i = 0; i < 257; i++)
+}
+*/
